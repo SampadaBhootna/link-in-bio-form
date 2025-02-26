c35623bb071f0a1fe5acf200ac33cd7272215e38
SampadaBhootna <sbhootna@chicagobooth.edu>
2025-02-26 16:07:22 +0000

    Dynamic Link in Bio Form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def admin
+    render ({ :template => "item_templates/admin"})
+  end
+
+  def insert_item
+    @x = Item.new
+    @x.link_url = params.fetch(:query_link_url)
+    @x.link_description = params.fetch(:query_link_description)
+    @x.thumbnail_url = params.fetch(:query_thumbnail_url)
+    @x.save
+    redirect_to("/")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "admin"})
+
+  post("/insert_item", { :controller => "items", :action => "insert_item"})
 end
